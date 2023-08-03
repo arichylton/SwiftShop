42fe5ab2c19d65acb92722628b14dc7f81c9460f
Aric Hylton <arich212@gmail.com>
2023-08-03 14:58:18 -0500

    first commit

---
+++ server/firebase.utils.js
@@ -0,0 +1,59 @@
+const { initializeApp } = require('firebase/app');
+
+const {
+  getFirestore,
+  doc,
+  collection,
+  getDocs,
+  getDoc,
+  setDoc,
+  Firestore,
+} = require('firebase/firestore');
+
+// Your web app's Firebase configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: `${process.env.FIREBASE_API_KEY}`,
+  authDomain: 'swiftshop-db.firebaseapp.com',
+  projectId: 'swiftshop-db',
+  storageBucket: 'swiftshop-db.appspot.com',
+  messagingSenderId: '127152835913',
+  appId: '1:127152835913:web:9dd912b386da2343141e98',
+  measurementId: 'G-8EK1MYVLHG',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+const getAllProducts = async () => {
+  const querySnapshot = await getDocs(collection(db, 'products'));
+
+  let products = [];
+
+  try {
+    querySnapshot.forEach((doc) => {
+      products.push(doc.data());
+    });
+    return products;
+  } catch (error) {
+    console.log('error fetching products', error.message);
+  }
+};
+
+const getProductData = async (currentItemUID) => {
+  const docRef = doc(db, 'products', currentItemUID);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists) {
+    try {
+      return docSnap.data();
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+};
+
+module.exports = { db, getAllProducts, getProductData };
---
+++ server/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const { resolve } = require('path');
+// Replace if using a different env file or config
+const env = require('dotenv').config({ path: './.env' });
+const { getAllProducts } = require('./firebase.utils');
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2022-08-01',
+});
+
+app.use(express.static(process.env.STATIC_DIR));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  const path = resolve(process.env.STATIC_DIR + '/index.html');
+  res.sendFile(path);
+});
+
+app.get('/config', (req, res) => {
+  res.send({
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  });
+});
+
+app.get('/products-data', async (req, res) => {
+  const productsDataInfo0 = await getAllProducts();
+
+  res.send({ productsDataInfo });
+});
+
+app.post('/create-payment-intent', async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: 'usd',
+      amount: 10000,
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (err) {
+    return res.status(400).send({
+      error: {
+        message: err.message,
+      },
+    });
+  }
+});
+
+app.listen(5252, () =>
+  console.log(`Node server listening at http://localhost:5252`)
+);
