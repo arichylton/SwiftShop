e69b66c995c2c3e73345187852b9709d76f784c9
Aric Hylton <arich212@gmail.com>
2023-10-04 17:11:28 -0500

    changed server path

--- server/server.js
+++ server/server.js
@@ -111,6 +111,8 @@ app.post('/create-payment-intent', async (req, res) => {
   }
 });
 
-app.listen(5252, () =>
-  console.log(`Node server listening at http://localhost:5252`)
+const PORT = process.env.PORT || 5252;
+
+app.listen(PORT, () =>
+  console.log(`Node server listening`)
 );
