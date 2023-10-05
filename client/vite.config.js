cbf3a30f549b6d9aa56648f514b2f4d1f3984900
Aric Hylton <arich212@gmail.com>
2023-10-05 14:10:27 -0500

    changed redirect uri

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-let REDIRECT_URI = 'https://swiftshop-a17bbc679c10.herokuapp.com';
+let REDIRECT_URI = 'http://swiftshop-a17bbc679c10.herokuapp.com';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
