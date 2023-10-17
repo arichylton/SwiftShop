5773318269559d38584f701d00af001c9c648ce3
Aric Hylton <arich212@gmail.com>
2023-10-17 02:26:21 -0500

    changed netlify.toml

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,19 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '', // Adjust to match your Heroku app name
-  build: {
-    outDir: '../server/public', // Output directory relative to the client directory
-    assetsDir: '.', // Assets directory relative to the outDir
-  },
-  server: {
-    port: 4242,
-    // client/vite.config.js
-    proxy: {
-      '/config': 'http://localhost:5252',
-      '/create-payment-intent': 'http://localhost:5252',
-      '/products-data': 'http://localhost:5252',
-      '/update-payment-intent': 'http://localhost:5252',
-    },
-  },
 });
