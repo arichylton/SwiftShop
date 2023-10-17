db99b1f5296c463b23b6b8c17c0f60e9c76a983b
Aric Hylton <arich212@gmail.com>
2023-10-17 16:51:07 -0500

    change vite.config

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  'import.meta.env.VITE_API_KEY': import.meta.env.VITE_API_KEY,
 });
